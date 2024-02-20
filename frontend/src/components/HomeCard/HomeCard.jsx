e25df1c4dd2e4a264bb3244b913803a01e04b7dc
JSLSasiRekha <105384982+JSLSasiRekha@users.noreply.github.com>
2024-02-20 23:27:10 +0530

    home page modified

--- frontend/src/components/HomeCard/HomeCard.jsx
+++ frontend/src/components/HomeCard/HomeCard.jsx
@@ -13,7 +13,7 @@ const Card = (props) => {
         boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2) 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
       }} className="rounded-2xl flex flex-col items-center p-5 gap-y-4">
 
-        <Counter number={Number(props.number)} />
+        <Counter to={Number(props.number)} />
 
         <button style={{ height: "50px", width: "200px", padding: "10px 30px", backgroundColor: "rgb(30 64 175)", color: "white", borderRadius: "4px", border: "none", cursor: "pointer" }}>
           {props.type}
