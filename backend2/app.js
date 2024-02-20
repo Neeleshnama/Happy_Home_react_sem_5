c6fbdcdbdc2a7a765d28bad688b85a19ecd6e2c3
Neelesh <neeleshnama2002@gmail.com>
2024-02-20 09:16:15 +0530

    Morgan Implemented

--- backend2/app.js
+++ backend2/app.js
@@ -4,6 +4,11 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const rfs = require('rotating-file-stream');
+const moment = require('moment');
 
 app.use(cors({
   origin: ['http://localhost:3000',],
@@ -16,6 +21,7 @@ app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
 
+
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 // config
@@ -25,12 +31,37 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   });
 }
 
+
+// using morgan middleware 
+// Create a rotating write stream
+const time=Date()
+const logDirectory = path.join(__dirname, 'logs');
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+const accessLogStream = rfs.createStream((time, index) => {
+  //if (!time) return 'access.log'; // default file name
+  const currentDate = moment().format('YYYY-MM-DD');
+  return `${currentDate}-access.log`; // file name with date
+}, {
+  interval: '1d', // rotate daily
+  path: logDirectory,
+});
+
+// Custom token to include request URL and method in log
+morgan.token('custom', (req, res) => {
+  return `a request to ${req.url} arrived on ${moment().format('YYYY-MM-DD HH:mm:ss')}`;
+});
+
+// Morgan middleware setup to log requests to a file with custom format
+app.use(morgan(':custom and this is method :method server took :response-time to reply this request with a status code :status', { stream: accessLogStream }));
+
+
 // import routes
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
+const { timeStamp } = require("console");
 
 app.use("/api/v2/order", order);
 app.use("/api/v2/user", user);
--- frontend/src/pages/AdminDashboardOrders.jsx
+++ frontend/src/pages/AdminDashboardOrders.jsx
@@ -60,7 +60,7 @@ const AdminDashboardOrders = () => {
       row.push({
         id: item._id,
         itemsQty: item?.cart?.reduce((acc, item) => acc + item.qty, 0),
-        total: item?.totalPrice + " $",
+        total: item?.totalPrice + " ₹",
         status: item?.status,
         createdAt: item?.createdAt.slice(0,10),
       });
